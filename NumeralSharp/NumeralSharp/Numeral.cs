6486b6871eae5688f0d43b37d42bedb902bc6ce0
Ian Kenley <ikenley6@gmail.com>
2017-08-27 07:09:41 -0400

    visual studio scaffolding

---
+++ NumeralSharp/NumeralSharp/Numeral.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NumeralSharp
+{
+    public class Numeral
+    {
+        public double? Value { get; set; }
+
+        public string Format(string format)
+        {
+            return Value.ToString();
+        }
+
+        public Numeral(double? value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ NumeralSharp/TestNumeral/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NumeralSharp;
+
+namespace TestNumeral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numeral = new Numeral(0.47351);
+            var formattedNum = numeral.Format("0.00%");
+            Console.WriteLine(formattedNum); //"47.35%"
+        }
+    }
+}
